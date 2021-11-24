ee4e1bb182954513060fc0c7f78207a889af08ae
rinat <50762155+mahmtfs@users.noreply.github.com>
2021-11-24 23:40:30 +0200

    Update logs.py

--- logger/logs.py
+++ logger/logs.py
@@ -6,4 +6,4 @@ logger.basicConfig(filename='logs/tests.log',
                    format='%(asctime)s - %(levelname)s - %(message)s',
                    level=logging.ERROR)
 
-BASE = 'http://0.0.0.0:8080/'
+BASE = 'http://127.0.0.1:5000/'
