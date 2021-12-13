51d0e31478e13f6472a8b40cf01289aa4981c5e3
rinat <50762155+mahmtfs@users.noreply.github.com>
2021-12-13 08:12:08 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ import secrets
 DEBUG = False
 TESTING = False
 SECRET_KEY = 'dopy'
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = f'postgresql{os.getenv('DATABASE_URL')[8:]}'
+
 
 MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 465
