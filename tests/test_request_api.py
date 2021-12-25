a6edb2a1de0b8c14922d106b248b15e41470fe14
rinat <50762155+mahmtfs@users.noreply.github.com>
2021-12-25 15:29:49 +0200

    Update test_request_api.py

--- tests/test_request_api.py
+++ tests/test_request_api.py
@@ -2,24 +2,25 @@ import requests
 import pytest
 import jwt
 import datetime
-from app import app
+from rest import app
 from logger.logs import logger
-from config import URL
 
 
 req_id = None
 token = jwt.encode({'id': 1, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=10)},
                    app.config['SECRET_KEY'], algorithm='HS256')
+URL = ''
+client = app.test_client()
 
 
 def test_post():
     try:
-        response = requests.post(f'{URL}/req', json={'token': token,
+        response = client.post(f'{URL}/req', json={'token': token,
                                                      'sender': 2,
                                                      'change_department_id': 1,
                                                      'increase_salary': 20})
         global req_id
-        req_id = response.json()['id']
+        req_id = response.json['id']
         assert response.status_code == 200
     except Exception as e:
         logger.error(f'Login test failed ({type(e).__name__}:{e})')
@@ -29,7 +30,7 @@ def test_post():
 
 def test_get():
     try:
-        response = requests.get(f'{URL}/req/{req_id}', json={'token': token})
+        response = client.get(f'{URL}/req/{req_id}', json={'token': token})
         assert response.status_code == 200
     except Exception as e:
         logger.error(f'Login test failed ({type(e).__name__}:{e})')
@@ -39,7 +40,7 @@ def test_get():
 
 def test_patch():
     try:
-        response = requests.patch(f'{URL}/req/{req_id}', json={'token': token,
+        response = client.patch(f'{URL}/req/{req_id}', json={'token': token,
                                                                'status': 2})
         assert response.status_code == 200
     except Exception as e:
@@ -50,7 +51,7 @@ def test_patch():
 
 def test_get_fail():
     try:
-        response = requests.get(f'{URL}/req/0', json={'token': token})
+        response = client.get(f'{URL}/req/0', json={'token': token})
         assert response.status_code == 404
     except Exception as e:
         logger.error(f'Login test failed ({type(e).__name__}:{e})')
@@ -60,7 +61,7 @@ def test_get_fail():
 
 def test_get_requests():
     try:
-        response = requests.get(f'{URL}/reqs', json={'token': token})
+        response = client.get(f'{URL}/reqs', json={'token': token})
         assert response.status_code == 200
     except Exception as e:
         logger.error(f'Sign in test failed ({type(e).__name__}:{e})')
