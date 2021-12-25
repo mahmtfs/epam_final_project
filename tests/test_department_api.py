bb3fb47a54b1f41d9e8d18fe23d671effb933cc6
rinat <50762155+mahmtfs@users.noreply.github.com>
2021-12-25 14:31:54 +0200

    Update test_department_api.py

--- tests/test_department_api.py
+++ tests/test_department_api.py
@@ -39,7 +39,7 @@ def test_get():
 
 def test_patch():
     try:
-        response = client.patch(f'/dep/{dep_id}', json={'token': token,
+        response = client.patch(f'http://127.0.0.1:5000/dep/{dep_id}', json={'token': token,
                                                                'title': 'test2'})
         assert response.status_code == 200
     except Exception as e:
@@ -50,7 +50,7 @@ def test_patch():
 
 def test_get_fail():
     try:
-        response = client.get(f'/dep/0', json={'token': token})
+        response = client.get(f'http://127.0.0.1:5000/dep/0', json={'token': token})
         assert response.status_code == 404
     except Exception as e:
         logger.error(f'Login test failed ({type(e).__name__}:{e})')
@@ -60,7 +60,7 @@ def test_get_fail():
 
 def test_delete():
     try:
-        response = client.delete(f'/dep/{dep_id}', json={'token': token})
+        response = client.delete(f'http://127.0.0.1:5000/dep/{dep_id}', json={'token': token})
         assert response.status_code == 200
     except Exception as e:
         logger.error(f'Delete test failed ({type(e).__name__}:{e})')
